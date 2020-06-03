81f8106ab9ed670e31eac2bc9e4fac72ca2cdcad
German Nunez <german90nunez@gmail.com>
2020-06-02 23:01:45 -0700

    componets styled and created

---
+++ src/components/AddTransaction.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const AddTransaction = () => {
+    return (
+        <div>
+           <h3>Add new transaction</h3>
+           <div className="form-control">
+               <label htmlFor="text">Text</label>
+               <input type="text" placeholder="Renter text..." />
+           </div>
+           <div className="form-control">
+               <label htmlFor="amount">Amount <br/> (negative - expense, positive - income)</label>
+               <input type="number" placeholder="Enter amount..."/>
+           </div>
+           <button className="btn">Add transaction</button>
+        </div>
+    )
+}
+
+export default AddTransaction
