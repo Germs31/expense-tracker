ce2e1ac146e1e1f16558b2d2b0c751db71d7153f
German Nunez <german90nunez@gmail.com>
2020-06-03 12:16:11 -0700

    transactions colored according to expense amount

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,10 +1,11 @@
 import React from 'react'
 
 const Transaction = ({transaction}) => {
+    const sign = transaction.amount < 0 ? '-' : '+';
     return (
         <div>
-            <li className="minus">
-                {transaction.amount} <span>-$400</span><button className="delete-btn">X</button>
+            <li className={transaction.amount < 0 ? 'minus' : 'plus'}>
+                {transaction.amount} <span>{sign}${Math.abs(transaction.amount)}</span><button className="delete-btn">X</button>
             </li>
         </div>
     )
