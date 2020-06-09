4d44f83dfc290475ac7bb4d2ff7192a720d1cac6
German Nunez <german90nunez@gmail.com>
2020-06-08 20:34:09 -0700

    db connected, routes and controller tested

---
+++ controllers/transactionsController.js
@@ -0,0 +1,9 @@
+exports.getTransactions = (req,res,next) => {
+    res.send('GET transactions')
+};
+exports.addTransactions = (req,res,next) => {
+    res.send('POST transactions')
+};
+exports.deleteTransactions = (req,res,next) => {
+    res.send('DELETE transactions')
+};
